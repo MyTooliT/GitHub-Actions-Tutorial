da6bb45cbcc5ad2254b93fdefcf2009ef3d43449
René Schwaiger <sanssecours@me.com>
2023-05-25 11:21:03 +0200

    CI: Check code with two tools in parallel

--- source.py
+++ source.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+def add(*numbers: float) -> None:
+    print(f"{' + '.join(map(str, numbers))} = {sum(numbers)}")
+
+
+add(1, 2, 3)
+add(4, 5, "6")  # Argument 3 has incorrect type
